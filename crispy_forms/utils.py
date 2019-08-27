2ef6412a1cde583649e9576849e25e220b2bb271
Charl Joseph Mert <charl.mert@gmail.com>
2019-08-27 08:28:36 +0200

    Accepting renderer=None for Django 2.2.4 cruds support

--- crispy_forms/utils.py
+++ crispy_forms/utils.py
@@ -35,11 +35,11 @@ def widget_get_context(name, value, attrs={}):
 
 def set_field_html_name(cls, new_name):
     """
-    This creates wrapper around the normal widget rendering, 
+    This creates wrapper around the normal widget rendering,
     allowing for a custom field name (new_name).
     """
     old_render = cls.widget.render
-    def _widget_render_wrapper(name, value, attrs=None):
+    def _widget_render_wrapper(name, value, attrs=None, renderer=None):
         return old_render(new_name, value, attrs)
 
     cls.widget.render = _widget_render_wrapper
